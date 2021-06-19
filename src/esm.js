1b5cf2f42c4bac14ea6ab89e91623a20b67985f9
Ryan Carniato <ryansolid@gmail.com>
2021-06-19 00:42:13 -0700

    update for 1.0

--- src/esm.js
+++ src/esm.js
@@ -4,13 +4,17 @@ export default function hot(Comp, isHot) {
   let _$Component = Comp;
   function _$handler(newModule) {
     newModule.$HotComponent.setComp = Comp.setComp;
-    Comp.setComp(newModule.$HotComponent);
+    Comp.setComp(() => newModule.$HotComponent);
   }
   if (isHot) {
     const [comp, setComp] = createSignal(Comp);
     Comp.setComp = setComp;
     let c;
-    _$Component = props => createMemo(() => (c = comp()) && untrack(() => c(props)));
+    _$Component = new Proxy(props => createMemo(() => (c = comp()) && untrack(() => c(props))), {
+      get(_, property) {
+        return comp()[property];
+      }
+    });
   }
   return { _$Component, _$handler };
 }
--- src/standard.js
+++ src/standard.js
@@ -5,12 +5,16 @@ export default function hot(Comp, hot) {
     const [comp, setComp] = createSignal(Comp);
     const prev = hot.data;
     if (prev && prev.setComp) {
-      prev.setComp(Comp);
+      prev.setComp(() => Comp);
     }
     hot.dispose(data => (data.setComp = prev ? prev.setComp : setComp));
     hot.accept();
     let c;
-    return props => createMemo(() => (c = comp()) && untrack(() => c(props)));
+    return new Proxy(props => createMemo(() => (c = comp()) && untrack(() => c(props))), {
+      get(_, property) {
+        return comp()[property];
+      }
+    });
   }
   return Comp;
 }
