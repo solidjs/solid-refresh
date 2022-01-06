75c00620bc47591a0df4e3d7dc44ef1a2f05bbf3
Artem Khodyush <greenkaa@gmail.com>
2022-01-06 00:56:58 -0800

    wrap component for HMR only if it went through devComponent (#10)
    * wrap component for HMR only if it went through devComponent
    
    * update symbol name for dev components to match the new name in solid.js

--- src/create-proxy.ts
+++ src/create-proxy.ts
@@ -1,4 +1,4 @@
-import { JSX, createMemo, untrack } from 'solid-js';
+import { JSX, createMemo, untrack, $DEVCOMP } from 'solid-js';
 
 interface BaseComponent<P> {
   (props: P): JSX.Element;
@@ -7,15 +7,20 @@ interface BaseComponent<P> {
 export default function createProxy<C extends BaseComponent<P>, P>(
   source: () => C,
 ): (props: P) => JSX.Element {
-  return new Proxy((props: P) => (
-    createMemo(() => {
-      const c = source();
-      if (c) {
-        return untrack(() => c(props));
-      }
-      return undefined;
-    })
-  ), {
+  return new Proxy(function hmrCompWrapper(props: P, ...rest) {
+    const s = source();
+    if (!s || $DEVCOMP in s) {
+      return createMemo(() => {
+        const c = source();
+        if (c) {
+          return untrack(() => c(props));
+        }
+        return undefined;
+      });
+    }
+    // no $DEVCOMP means it did not go through devComponent so source() is a regular function, not a component
+    return s.call(this, props, ...rest);
+  }, {
     get(_, property: keyof C) {
       return source()[property];
     },
