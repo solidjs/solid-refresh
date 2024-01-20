8fb17d8968ee6adc2ac27e758a8152d3e84426e1
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2024-01-20 15:52:30 +0800

    Add playground

---
+++ playground/src/App.tsx
@@ -0,0 +1,29 @@
+// @refresh granular
+
+import { createSignal, onCleanup, onMount } from 'solid-js';
+
+export default function App() {
+  const [count, setCount] = createSignal(0);
+
+  function increment() {
+    setCount(c => c + 1);
+  }
+
+  function decrement() {
+    setCount(c => c - 1);
+  }
+  onMount(() => {
+    console.log('Mounted App');
+  });
+  onCleanup(() => {
+    console.log('Unmounted App');
+  });
+
+  return (
+    <>
+      <h1>Counts: {count()}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  );
+}
